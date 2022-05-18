ebe060381a116a3fde54d69557f0f314a6af2200
CaioSC01 <caio180g@gmail.com>
2022-05-18 09:30:21 -0300

    front-end init

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// Estilização ao todo no site 
+import './style/global.scss';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
